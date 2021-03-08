bef1bfeea360402db945c25a7af6dee442211120
zhengis-alinur <zhengis.alinur.erzhan@gmail.com>
2021-03-09 03:32:21 +0600

    init: find solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if((typeof str == 'undefined') || (typeof bracketsConfig == 'undefined')) {
+    return false;
+  }
+  if (str.length % 2 != 0 || str.length == 0) {
+    return false;
+  }
+  closeBrackets = new Array();
+  openBrackets = new Array();
+  for(i = 0; i < bracketsConfig.length; i++) {
+    for(j = 0; j < bracketsConfig[i].length; j++) {
+      if(j == 0) {
+        openBrackets.push(bracketsConfig[i][j]);
+      } else {
+        closeBrackets.push(bracketsConfig[i][j]);
+      }
+    }
+  }
+  x = openBrackets.length*str.length/2;
+  function deleteDeepestBrackets(string) {
+    result = string;
+    if(result.length == 0 || x == 0) {
+      str = result;
+      return null;
+    }
+    for(i = 0; i < openBrackets.length; i++) {
+      let substr = openBrackets[i]+closeBrackets[i];
+      let index = string.indexOf(substr);
+      x--;
+      if(index !== -1) {
+        result = result.replace(substr, "");
+      }
+    }
+    deleteDeepestBrackets(result);
+  }
+  deleteDeepestBrackets(str);
+  return str.length == 0 ? true : false;
 }
